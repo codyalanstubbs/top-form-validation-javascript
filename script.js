5df20eb57076ce39f5f944d4cb907da8c1d2f6eb
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-01 13:33:38 -0400

    Add function to insert input specific messages to help users enter valid data

--- script.js
+++ script.js
@@ -2,18 +2,54 @@ const allInputs = document.querySelectorAll("input");
 const instructions = document.querySelector(".instructions");
 const submitBtn = document.querySelector("#submit");
 const email = document.getElementById("mail");
+const zipcode = document.getElementById("zipcode");
 
-email.addEventListener("input", (e) => {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("Enter a valid email address like john@company.com");
-        email.reportValidity();
-    } else {
-        email.setCustomValidity("");
-    }
+const simpleInputs = [
+    {element: email, message: "Please enter a valid email address like: john@email.com"},
+    {element: zipcode, message: "Please only use numbers"}
+]
+
+simpleInputs.forEach((inputObject) => {
+    const input = inputObject.element;
+    const inputMessage = inputObject.message;
+
+    input.addEventListener("input", (e) => {
+        const message = document.querySelector(`.message.${input.id}`)
+
+        if (checkInputInvalid(input)) {
+            if (!message) {
+                input.classList.add("empty");
+                addInputIcon(input, "⚠️");
+                insertInputInstructions(input, inputMessage)
+            }
+        } else {
+            if (message) { 
+                message.remove();
+                addInputIcon(input, "✔️");
+             }
+        }
+    })
 })
 
+function insertInputInstructions(inputElement, messageText) {
+    const inputContainerMail = document.querySelector(`.inputContainer.${inputElement.id}`);
+    
+    const message = document.createElement("div");
+    message.textContent = messageText;
+    message.classList = `message ${inputElement.id}`;
+
+    inputContainerMail.parentElement.insertBefore(message, inputContainerMail.nextElementSibling)
+}
+
 allInputs.forEach((inputElement) => {
-    inputElement.addEventListener("focusout", (e) => checkInputValidFocusOut(inputElement))
+    inputElement.addEventListener("focusout", (e) => {
+        if (checkInputInvalid(inputElement)) {
+            inputElement.classList.add("empty");
+            addInputIcon(inputElement, "⚠️");
+        } else {
+            addInputIcon(inputElement, "✔️");
+        }
+    })
 })
 
 submitBtn.addEventListener("click", (e) => {
@@ -21,17 +57,13 @@ submitBtn.addEventListener("click", (e) => {
     checkInputsEmpty(allInputs);
 });
 
-function checkInputValidFocusOut(inputElement) {
+function checkInputInvalid(inputElement) {
     if (
         inputElement.validity.typeMismatch ||
         inputElement.validity.patternMismatch ||
         inputElement.value === ''
     ) {
-        inputElement.classList.add("empty");
-        addInputIcon(inputElement, "⚠️");
-    } else {
-        console.log(inputElement.validity);
-        addInputIcon(inputElement, "✔️");
+        return true;
     }
 }
 
